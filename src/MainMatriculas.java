c93f301d56be760414979a13d4bc380ac5c4d3b9
enaut <enaut.irakasle@gmail.com>
2018-02-16 14:02:36 +0100

    primer commit con errores

---
+++ src/MainMatriculas.java
@@ -0,0 +1,12 @@
+
+public class MainMatriculas {
+
+	public static void main(String[] args) {
+		
+		MatriculaModelo matriculaModelo = new MatriculaModelo();
+		ArrayList<Matricula> matriculas = matriculaModelo.selectAll();
+		
+
+	}
+
+}
