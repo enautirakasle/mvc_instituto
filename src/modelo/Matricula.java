82cbb496aef5fd5debf4ebe664c817ebb803cd75
enaut <enaut.irakasle@gmail.com>
2018-02-19 08:57:35 +0100

    selectAll y select

--- src/modelo/Matricula.java
+++ src/modelo/Matricula.java
@@ -1,5 +1,32 @@
 package modelo;
 
-public class Matricula {
+import java.util.Date;
 
+public class Matricula {
+	
+	private int id_asignatura;
+	private int id_alumno;
+	private Date fecha;
+	
+	
+	public int getId_asignatura() {
+		return id_asignatura;
+	}
+	public void setId_asignatura(int id_asignatura) {
+		this.id_asignatura = id_asignatura;
+	}
+	public int getId_alumno() {
+		return id_alumno;
+	}
+	public void setId_alumno(int id_alumno) {
+		this.id_alumno = id_alumno;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+	
 }
