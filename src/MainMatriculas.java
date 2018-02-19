29d0fc79d4f35f17a0da88dd156b08c3ffadb955
enaut <enaut.irakasle@gmail.com>
2018-02-19 09:03:06 +0100

    pruba de los modelos para listar matriculas

--- src/MainMatriculas.java
+++ src/MainMatriculas.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import modelo.Alumno;
+import modelo.AlumnoModelo;
+import modelo.Asignatura;
+import modelo.AsignaturaModelo;
+import modelo.Matricula;
+import modelo.MatriculaModelo;
 
 public class MainMatriculas {
 
@@ -5,8 +14,19 @@ public class MainMatriculas {
 		
 		MatriculaModelo matriculaModelo = new MatriculaModelo();
 		ArrayList<Matricula> matriculas = matriculaModelo.selectAll();
+		AsignaturaModelo asignaturaModelo = new AsignaturaModelo();
+		AlumnoModelo alumnoModelo = new AlumnoModelo();
 		
-
+		Iterator<Matricula> i = matriculas.iterator();
+		while(i.hasNext()){
+			Matricula matricula = i.next();
+			Asignatura asignatura = asignaturaModelo.getAsignatura(matricula.getId_asignatura());
+			Alumno alumno = alumnoModelo.getAlumno(matricula.getId_alumno());
+			
+			System.out.println(alumno.getNombre() + " : " + alumno.getDni() + " : " + asignatura.getNombre() + " : "
+					+ asignatura.getHoras() + " : " + matricula.getFecha());
+			
+		}
 	}
 
 }
